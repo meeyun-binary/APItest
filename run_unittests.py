badc7ba8adc81501a652fdbad846c08913a0ac7c
meeyun <31909236+meeyun@users.noreply.github.com>
2018-03-04 22:05:57 +0800

    Update run_unittests.py

--- run_unittests.py
+++ run_unittests.py
@@ -4,8 +4,6 @@ import unittest
 
 
 sys.path.append(os.path.join(sys.path[0],'..'))
-# sys.path.append(os.path.join(sys.path[0],'testcases'))
-# test_dir = os.path.join(sys.path[0],'testcases')
 test_dir = os.path.join(sys.path[0])
 suite = unittest.TestLoader().discover(start_dir=test_dir,pattern='TestBuy.py')
 #unittest.TextTestRunner(verbosity=1).run(suite)
