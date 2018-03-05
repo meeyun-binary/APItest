b1965842fc4a50e3a50b94ee44989652497e4ffa
meeyun <31909236+meeyun@users.noreply.github.com>
2018-03-05 09:46:36 +0800

    Update run_unittests.py

--- run_unittests.py
+++ run_unittests.py
@@ -5,14 +5,18 @@ import unittest
 
 sys.path.append(os.path.join(sys.path[0],'..'))
 test_dir = os.path.join(sys.path[0])
-suite = unittest.TestLoader().discover(start_dir=test_dir,pattern='TestBuy.py')
+suite = unittest.TestLoader().discover(start_dir=test_dir,pattern='Test*.py')
 #unittest.TextTestRunner(verbosity=1).run(suite)
 
 ret = unittest.TextTestRunner(verbosity=1).run(suite).wasSuccessful()
 
+# ret is True(1) when test passes. 
+# so change exit code to 0 when test passes
+# thus, travis build will indicate build pass with exit code 0
 if ret is True:
     sys.exit(0)
 
+# travis build will should show fail when exit code is non-zero
 else:
     sys.exit(1)
 
