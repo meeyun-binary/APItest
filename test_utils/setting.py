18dced541794c34ab6946bc4154a306dccd3308b
meeyun <31909236+meeyun@users.noreply.github.com>
2018-03-13 15:48:45 +0800

    setting.py to support arg from command line

---
+++ test_utils/setting.py
@@ -0,0 +1,30 @@
+# If there is no argument given, by default, it will use production server and production token
+# To run in production: python run_unittests.py
+# To run in QA: python run_unittests.py -s "www.binaryqa27.com" -a 1003 -t "lmmtw76c5gGAwvq"
+
+# Currently travis.yml is set to production (python run_unittests.py)
+# we need to manually change it if need the test run in QA
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# add QA
+parser.add_argument('-s', action='store',
+                    dest='server',
+                    help="Testing server. E.g. 'www.binaryqa20.com'",
+                    default = "ws.binaryws.com")
+# add App ID
+parser.add_argument('-a', action='store',
+                    dest='app_id',
+                    help="App ID. E.g. 1003",
+                    default = 1089)
+# add token
+parser.add_argument('-t', action='store',
+                    dest='token',
+                    help="App token E.g. 'ei5EBsqQan230MG'",
+                    default = "ei5EBsqQan230MG")
+
+args = parser.parse_args()
+
