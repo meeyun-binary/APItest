3475d0aba3c57f39893800c990f0db85d837745d
meeyun <31909236+meeyun@users.noreply.github.com>
2018-03-22 17:57:20 +0800

    add print if error

--- testcases/TestBuyForex.py
+++ testcases/TestBuyForex.py
@@ -18,19 +18,6 @@ class TestBuyContractForex(unittest.TestCase):
         tu.sell_last_bought_contract()
         return
 
-    # rate limit for virtual buy is 20 per minute
-    @tu.rate_limited(0.35)
-    def buy_and_compare_longcode(self, proposal, expected_longcode):
-        id = proposal['proposal']['id']
-
-        json_buy = json.dumps({"buy": id,
-                               "price": 100
-                               })
-        result_buy = tu.send_and_receive_ws(json_buy)
-        longcode = result_buy['buy']['longcode']
-
-        self.assertEqual(longcode, expected_longcode)
-
     def test_buy_call_contract(self):
         proposal = tu.proposal_call_put(symbol=self.symbol,
                                         contract_type="CALL",
@@ -40,18 +27,18 @@ class TestBuyContractForex(unittest.TestCase):
         expected_longcode = 'Win payout if AUD/JPY is strictly higher than entry spot at close on ' \
                             '{}.'.format(self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
-
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_put_contract(self):
         proposal = tu.proposal_call_put(symbol=self.symbol,
                                         contract_type="PUT",
                                         duration=self.duration,
                                         duration_unit="d")
+
         expected_longcode = 'Win payout if AUD/JPY is strictly lower than entry spot at close on ' \
                             '{}.'.format(self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_lower_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -63,10 +50,11 @@ class TestBuyContractForex(unittest.TestCase):
                                             duration=self.duration,
                                             duration_unit="d",
                                             barrier=current_spot)
+
         expected_longcode = 'Win payout if AUD/JPY is strictly lower than {} at close on ' \
                             '{}.'.format(current_spot_formatted, self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_higher_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -78,10 +66,11 @@ class TestBuyContractForex(unittest.TestCase):
                                             duration=self.duration,
                                             duration_unit="d",
                                             barrier=current_spot)
+
         expected_longcode = 'Win payout if AUD/JPY is strictly higher than {} at close on ' \
                             '{}.'.format(current_spot_formatted, self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_touch_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -92,10 +81,11 @@ class TestBuyContractForex(unittest.TestCase):
                                               duration=self.duration,
                                               duration_unit="d",
                                               barrier=barrier)
+
         expected_longcode = 'Win payout if AUD/JPY touches {} through close on ' \
                             '{}.'.format(barrier, self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_no_touch_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -106,10 +96,11 @@ class TestBuyContractForex(unittest.TestCase):
                                               duration=self.duration,
                                               duration_unit="d",
                                               barrier=barrier)
+
         expected_longcode = 'Win payout if AUD/JPY does not touch {} through close on ' \
                             '{}.'.format(barrier, self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_end_between_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -122,10 +113,11 @@ class TestBuyContractForex(unittest.TestCase):
                                       duration_unit="d",
                                       barrier=barrier,
                                       barrier2=barrier2)
+
         expected_longcode = 'Win payout if AUD/JPY ends strictly between {} to {} at close on ' \
                             '{}.'.format(barrier2, barrier, self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_end_out_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -138,10 +130,11 @@ class TestBuyContractForex(unittest.TestCase):
                                       duration_unit="d",
                                       barrier=barrier,
                                       barrier2=barrier2)
+
         expected_longcode = 'Win payout if AUD/JPY ends outside {} to {} at close on ' \
                             '{}.'.format(barrier2, barrier, self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_stay_between_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -154,10 +147,11 @@ class TestBuyContractForex(unittest.TestCase):
                                       duration_unit="d",
                                       barrier=barrier,
                                       barrier2=barrier2)
+
         expected_longcode = 'Win payout if AUD/JPY stays between {} to {} through close on ' \
                             '{}.'.format(barrier2, barrier, self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_goes_outside_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -170,9 +164,10 @@ class TestBuyContractForex(unittest.TestCase):
                                       duration_unit="d",
                                       barrier=barrier,
                                       barrier2=barrier2)
+
         expected_longcode = 'Win payout if AUD/JPY goes outside {} to {} through close on ' \
                             '{}.'.format(barrier2, barrier, self.end_date)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
 
