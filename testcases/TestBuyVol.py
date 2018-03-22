78d584969334c10e12b4d246f52e056b4cad22bc
meeyun <31909236+meeyun@users.noreply.github.com>
2018-03-22 17:58:53 +0800

    add print if error

--- testcases/TestBuyVol.py
+++ testcases/TestBuyVol.py
@@ -13,38 +13,25 @@ class TestBuyContract(unittest.TestCase):
         tu.sell_last_bought_contract()
         return
 
-    # rate limit for virtual buy is 20 per minute
-    @tu.rate_limited(0.35)
-    def buy_and_compare_longcode(self, proposal, expected_longcode):
-        id = proposal['proposal']['id']
-
-        json_buy = json.dumps({"buy": id,
-                               "price": 100
-                               })
-        result_buy = tu.send_and_receive_ws(json_buy)
-        longcode = result_buy['buy']['longcode']
-
-        self.assertEqual(longcode, expected_longcode)
-
     def test_buy_call_contract(self):
         proposal = tu.proposal_call_put(symbol=self.symbol,
                                         contract_type="CALL",
                                         duration=self.duration,
                                         duration_unit="m")
-        expected_longcode = 'Win payout if Volatility 100 Index is strictly higher than entry spot at 3 minutes after ' \
-                            'contract start time.'
+        expected_longcode = 'Win payout if Volatility 100 Index is strictly higher than entry spot at 3 minutes ' \
+                            'after contract start time.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_put_contract(self):
         proposal = tu.proposal_call_put(symbol=self.symbol,
                                         contract_type="PUT",
                                         duration=self.duration,
                                         duration_unit="m")
-        expected_longcode = 'Win payout if Volatility 100 Index is strictly lower than entry spot at 3 minutes after ' \
-                            'contract start time.'
+        expected_longcode = 'Win payout if Volatility 100 Index is strictly lower than entry spot at 3 minutes ' \
+                            'after contract start time.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_lower_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -58,7 +45,7 @@ class TestBuyContract(unittest.TestCase):
                                             barrier=current_spot)
         expected_longcode = 'Win payout if Volatility 100 Index is strictly lower than {} ' \
                             'at 3 minutes after contract start time.'.format(current_spot_formatted)
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_higher_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -73,8 +60,7 @@ class TestBuyContract(unittest.TestCase):
         expected_longcode = 'Win payout if Volatility 100 Index is strictly higher than {} ' \
                             'at 3 minutes after contract start time.'.format(current_spot_formatted)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
-
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_touch_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -89,7 +75,7 @@ class TestBuyContract(unittest.TestCase):
         expected_longcode = 'Win payout if Volatility 100 Index touches {} through 3 minutes ' \
                             'after contract start time.'.format(abs_barrier)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_no_touch_contract(self):
         current_spot = tu.find_latest_tick(self.symbol)
@@ -104,7 +90,7 @@ class TestBuyContract(unittest.TestCase):
         expected_longcode = 'Win payout if Volatility 100 Index does not touch {} through 3 ' \
                             'minutes after contract start time.'.format(abs_barrier)
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_end_between_contract(self):
         proposal = tu.proposal_in_out(symbol=self.symbol,
@@ -117,7 +103,7 @@ class TestBuyContract(unittest.TestCase):
         expected_longcode = 'Win payout if Volatility 100 Index ends strictly between entry spot minus 4.50 to ' \
                             'entry spot plus 3.50 at 3 minutes after contract start time.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_end_out_contract(self):
         proposal = tu.proposal_in_out(symbol=self.symbol,
@@ -130,7 +116,7 @@ class TestBuyContract(unittest.TestCase):
         expected_longcode = 'Win payout if Volatility 100 Index ends outside entry spot minus 4.50 to entry spot ' \
                             'plus 3.50 at 3 minutes after contract start time.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_stay_between_contract(self):
         proposal = tu.proposal_in_out(symbol=self.symbol,
@@ -142,7 +128,7 @@ class TestBuyContract(unittest.TestCase):
         expected_longcode = 'Win payout if Volatility 100 Index stays between entry spot minus 4.50 and entry spot ' \
                             'plus 3.50 through 3 minutes after contract start time.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_goes_outside_contract(self):
         proposal = tu.proposal_in_out(symbol=self.symbol,
@@ -154,74 +140,74 @@ class TestBuyContract(unittest.TestCase):
         expected_longcode = 'Win payout if Volatility 100 Index goes outside entry spot minus 25.60 and entry ' \
                             'spot plus 23.40 through 3 minutes after contract start time.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_asian_down_contract(self):
         proposal = tu.proposal_asian_up_down(self.symbol, "ASIAND")
         expected_longcode = 'Win payout if the last tick of Volatility 100 Index is strictly lower than the ' \
                             'average of the 5 ticks.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_asian_up_contract(self):
         proposal = tu.proposal_asian_up_down(self.symbol, "ASIANU")
         expected_longcode = 'Win payout if the last tick of Volatility 100 Index is strictly higher than the ' \
                             'average of the 5 ticks.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_digit_match_contract(self):
         proposal = tu.proposal_digit(self.symbol, "DIGITMATCH")
         expected_longcode = 'Win payout if the last digit of Volatility 100 Index is 5 after 5 ticks.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_digit_diff_contract(self):
         proposal = tu.proposal_digit(self.symbol, "DIGITDIFF")
         expected_longcode = 'Win payout if the last digit of Volatility 100 Index is not 5 after 5 ticks.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_digit_over_contract(self):
         proposal = tu.proposal_digit(self.symbol, "DIGITOVER")
         expected_longcode = 'Win payout if the last digit of Volatility 100 Index is strictly higher than 5 ' \
                             'after 5 ticks.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_digit_under_contract(self):
         proposal = tu.proposal_digit(self.symbol, "DIGITUNDER")
         expected_longcode = 'Win payout if the last digit of Volatility 100 Index is strictly lower than 5 ' \
                             'after 5 ticks.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_digit_even_contract(self):
         proposal = tu.proposal_digit_even_odd(self.symbol, "DIGITEVEN")
         expected_longcode = 'Win payout if the last digit of Volatility 100 Index is even after 5 ticks.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_digit_odd_contract(self):
         proposal = tu.proposal_digit_even_odd(self.symbol, "DIGITODD")
         expected_longcode = 'Win payout if the last digit of Volatility 100 Index is odd after 5 ticks.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_lookback_high_close_contract(self):
         proposal = tu.proposal_lookback(self.symbol, "LBFLOATPUT")
         expected_longcode = 'Win USD 1 times Volatility 100 Index\'s high minus close over the next 1 minute.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_lookback_close_low_contract(self):
         proposal = tu.proposal_lookback(self.symbol, "LBFLOATCALL")
         expected_longcode = 'Win USD 1 times Volatility 100 Index\'s close minus low over the next 1 minute.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
 
     def test_buy_lookback_high_low_contract(self):
         proposal = tu.proposal_lookback(self.symbol, "LBHIGHLOW")
         expected_longcode = 'Win USD 1 times Volatility 100 Index\'s high minus low over the next 1 minute.'
 
-        self.buy_and_compare_longcode(proposal, expected_longcode)
+        tu.buy_and_compare_longcode(proposal, expected_longcode)
