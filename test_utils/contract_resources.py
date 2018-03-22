b69669e45ca0189fd05d5192631a4369bcaddf2a
meeyun <31909236+meeyun@users.noreply.github.com>
2018-03-22 18:02:43 +0800

    add print if error

--- test_utils/contract_resources.py
+++ test_utils/contract_resources.py
@@ -1,6 +1,7 @@
 import json
 import test_utils as tu
 
+
 # from websocket import create_connection
 
 
@@ -21,6 +22,7 @@ def find_latest_tick(symbol):
     # note the current spot is float type.
     return current_spot
 
+
 def proposal_call_put(symbol, contract_type, duration, duration_unit):
     proposal = json.dumps({"proposal": 1,
                            "amount": "10",
@@ -34,6 +36,7 @@ def proposal_call_put(symbol, contract_type, duration, duration_unit):
     proposal_result_js = tu.send_and_receive_ws(proposal)
 
     return proposal_result_js
+
 
 def proposal_higher_lower(symbol, contract_type, barrier, duration, duration_unit):
     proposal = json.dumps({"proposal": 1,
@@ -50,6 +53,7 @@ def proposal_higher_lower(symbol, contract_type, barrier, duration, duration_uni
 
     return proposal_result_js
 
+
 def proposal_touch_no_touch(symbol, contract_type, barrier, duration, duration_unit):
     proposal = json.dumps({"proposal": 1,
                            "amount": "100",
@@ -65,6 +69,7 @@ def proposal_touch_no_touch(symbol, contract_type, barrier, duration, duration_u
     proposal_result_js = tu.send_and_receive_ws(proposal)
 
     return proposal_result_js
+
 
 def proposal_in_out(symbol, contract_type, barrier, barrier2, duration, duration_unit):
     proposal = json.dumps({"proposal": 1,
@@ -83,6 +88,7 @@ def proposal_in_out(symbol, contract_type, barrier, barrier2, duration, duration
 
     return proposal_result_js
 
+
 def proposal_asian_up_down(symbol, contract_type):
     proposal = json.dumps({"proposal": 1,
                            "amount": "10",
@@ -96,6 +102,7 @@ def proposal_asian_up_down(symbol, contract_type):
     proposal_result_js = tu.send_and_receive_ws(proposal)
 
     return proposal_result_js
+
 
 def proposal_digit(symbol, contract_type):
     proposal = json.dumps({"proposal": 1,
@@ -112,6 +119,7 @@ def proposal_digit(symbol, contract_type):
 
     return proposal_result_js
 
+
 def proposal_digit_even_odd(symbol, contract_type):
     proposal = json.dumps({"proposal": 1,
                            "amount": "10",
@@ -125,6 +133,7 @@ def proposal_digit_even_odd(symbol, contract_type):
     proposal_result_js = tu.send_and_receive_ws(proposal)
 
     return proposal_result_js
+
 
 def proposal_lookback(symbol, contract_type):
     proposal = json.dumps({"proposal": 1,
@@ -147,7 +156,7 @@ def sell_last_bought_contract():
 
     num_open_contract = len(result_contract_id['portfolio']['contracts'])
 
-    if(num_open_contract!=0):
+    if num_open_contract != 0:
         # get the contract id of last purchase contract
         contract_id = result_contract_id['portfolio']['contracts'][-1]['contract_id']
 
@@ -162,8 +171,8 @@ def sell_last_bought_contract():
         result_sell_contract = "No open contract. Skipped selling contract"
         print (result_sell_contract)
 
-
     return result_sell_contract
+
 
 def abs_higher_barrier(current_spot, barrier, decimal_places):
     # create absolute barriers
@@ -172,9 +181,31 @@ def abs_higher_barrier(current_spot, barrier, decimal_places):
 
     return abs_barrier_formatted
 
+
 def abs_lower_barrier2(current_spot, barrier2, decimal_places):
     # create absolute barriers
     abs_barrier2 = current_spot - barrier2
     abs_barrier_formatted2 = '{0:.{1}f}'.format(abs_barrier2, decimal_places)
 
     return abs_barrier_formatted2
+
+
+@tu.rate_limited(0.35)
+def buy_and_compare_longcode(proposal, expected_longcode):
+    print_if_error(proposal)
+    id = proposal['proposal']['id']
+
+    json_buy = json.dumps({"buy": id,
+                           "price": 100
+                           })
+    result_buy = tu.send_and_receive_ws(json_buy)
+
+    print_if_error(result_buy)
+
+    longcode = result_buy['buy']['longcode']
+    assert longcode, expected_longcode
+
+
+def print_if_error(call):
+    if 'error' in call:
+        print("{} error! \n {}".format(call['msg_type'], call))
