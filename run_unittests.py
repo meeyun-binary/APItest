134c61173d4ba510b2167da4a82662c2b79a9b97
meeyun <31909236+meeyun@users.noreply.github.com>
2018-05-28 14:18:39 +0800

    Update run_unittests.py

--- run_unittests.py
+++ run_unittests.py
@@ -5,7 +5,7 @@ import unittest
 
 sys.path.append(os.path.join(sys.path[0],'..'))
 test_dir = os.path.join(sys.path[0])
-suite = unittest.TestLoader().discover(start_dir=test_dir,pattern='TestForgetAll.py')
+suite = unittest.TestLoader().discover(start_dir=test_dir,pattern='Demo1.py')
 #unittest.TextTestRunner(verbosity=1).run(suite)
 
 ret = unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
